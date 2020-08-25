a68d3b60b76ef1ff661860d7ce73c63d2ce29071
Vitor Pouza <Victot@MacBook-Pro-de-Vitor.local>
2020-08-25 18:20:37 -0300

    Updates

---
+++ precache-manifest.7e049f72acbd4ff528b2a25a71111f62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87a8f823e0c8c311b3f252f7e4dfc62",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "2c5f923016be659981df",
+    "url": "/todolist/static/js/2.f5724139.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todolist/static/js/2.f5724139.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a752cf273b398ce0eaf5",
+    "url": "/todolist/static/js/main.38b61dc0.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
